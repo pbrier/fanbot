c4d6156f649383efe1aef543188188430c657018
pbrier <git@pbrier.nl>
2013-05-20 22:35:57 +0200

    added GNU header in mbed SW

--- software/mbed/fanbot/fanbot.cpp
+++ software/mbed/fanbot/fanbot.cpp
@@ -1,7 +1,23 @@
 /*
- * USB HID Example, with serial console (not using Serial object, cause this is too large
- * Peter Brier
- */
+
+Copyright (c) 2013 Peter Brier
+
+This file is part of the FanBot project.
+
+Fanbot is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
 
 #include "mbed.h"
 #include "USBHID.h"
@@ -30,21 +46,16 @@ PwmOut servo1(P0_18);
 PwmOut servo2(P0_19);
 Ticker tic;
 
+
 // fanbot IO
-BusOut leds(P1_19, P1_25, P0_8, P0_9, P0_22, P0_13, P0_14);
-/* DigitalOut led1(P1_19);
-DigitalOut led2(P1_25);
-DigitalOut led3(P0_8);
-DigitalOut led4(P0_9);
-DigitalOut led5(P0_22);
-DigitalOut led6(P0_13);
-DigitalOut led7(P0_14);
-*/
-DigitalIn  button(P0_1);
+BusOut leds(P1_19, P1_25, P0_8, P0_9, P0_22, P0_13, P0_14); // Leds
+DigitalIn  button(P0_1);   // Button
 DigitalIn  uart_rx(P1_26); // rx pin as input, so we can disable the pull-up before enumeration
 
+
 // IR led and receiver
-DigitalIn ir_rx(P1_16);
+// 1st 5 protos had IR on P1.16: DigitalIn ir_rx(P1_16);
+DigitalIn ir_rx(P1_14);
 DigitalOut ir_tx(P1_13);
 PwmOut ir_pwm(P1_13);
 
@@ -55,18 +66,34 @@ PwmOut ir_pwm(P1_13);
 IAP iap;
 
 #define FAN_STEPS 20 // nr of program steps
+unsigned short int program[128]; // buffer for EEPROM, 256 bytes, need to be word aligned
 
+// Globals
 volatile int read_res=0, send_res=0, counter=0;
+int serial_nr = 0;
 
-void tic_handler() {
-    send_report.data[2] = counter++;
-    send_res = hid.send(&send_report);	
+// USB send Tick handler, send a report (every msec)
+// The report format is:
+// 0: 0
+// 1: status code
+// 2: Counter
+// 3: Button state
+// 4: Servo 1 position
+// 5: Servo 2 position
+// 6: Led state
+// 7: n.a.
+// 8..11: Serial #
+void tic_handler() 
+{
+  send_report.data[2] = counter++;
+  memcpy(&send_report.data[8], &serial_nr, sizeof(serial_nr) );
+  send_res = hid.send(&send_report);	
 }
 
 // Software UART send. mark is LOW space is HIGH
 #define BAUD 9600
-#define t_bit (1000000/BAUD) // 9600 bps, 10 microseconds bit time
-void send(DigitalOut &p, char c)
+#define t_bit (1000000/BAUD) // 9600 bps, ~10 microseconds bit time
+void send(DigitalOut &p, const char c)
 {
   p = 1; // 1 start bit
   wait_us(t_bit);
@@ -79,75 +106,145 @@ void send(DigitalOut &p, char c)
   wait_us(t_bit);
 }
 
-void tx(DigitalOut &p, char *s)
+// Transmit a string in serial mode
+void tx(DigitalOut &p, const char *s)
 {
   wait_us(2000);
   while ( *s ) 
   {
     send(p, *s++);
-	wait_us(1000);
+    wait_us(1000);
   }
 }
 
+// send a single nibble
+void tx_nibble(DigitalOut &p, const int n)
+{
+  wait_us(2000);
+  send(p, "0123456789ABCDEF"[n & 0xF] );
+  wait_us(2000);
+}
+
+// Play the sequence
+void play()
+{
+  iap.read_eeprom( (char*)TARGET_ADDRESS, (char *)program, MEM_SIZE );
+  for(int i=0; i<FAN_STEPS; i++)
+  {
+    leds = program[i] & 0xFF;
+    servo1.pulsewidth_us(700 + (program[i]>>8 & 0xFF) );
+    wait(0.2);
+  }
+}
+
+// Set servo position
+void set_servo(char n, int val)
+{
+  if ( n == 'A' ) servo1.pulsewidth_us(700 + 25 * val);
+  if ( n == 'B' ) servo2.pulsewidth_us(700 + 25 * val);
+}
+
+// get value, wait max 30msec
+int get_val()
+{
+  char c = 0;
+  for(int i=0; i<30 && !serial_readable( &stdio_uart );i++ )
+    wait(0.001);
+  c = serial_getc(&stdio_uart); 
+  return (int)(c-'0');
+}
+
+/**
+*** run serial communication mode
+*** Commands:
+*** 'p' Play sequence
+*** 's' Stop
+*** 'L' [val]: select LED value
+*** 'A' [val]  arm position [servo 1] (0 is off)
+*** 'B' [val]  arm position [servo 2] (0 is off)
+*** 'n' Report serial nr (send as 8 digit HEX number)
+*** 'r' Read memory
+***
+**/
 void do_serial()
 {
   volatile static int i;
   DigitalOut usb_conn(P0_6); // we control the USB connect output now
-  //PwmOut *s1, *s2; 
-  pwmout_t _pwm;  // dummy PWM struct to re-init the PWM ports
+  pwmout_t _pwm;  // dummy PWM struct to re-init the PWM ports (reselects the pin functions after serial init)
    
   serial_init( &stdio_uart, P1_27, P1_26); // pins: tx, rx
   pin_mode( P1_26, PullNone); // disable weak pullup for RX
   // pin_mode( P0_6, PullNone); // disable weak pullup for USB_CON
   
-  // printf("Start!\n");
   pwmout_init(&_pwm, P0_18);
   pwmout_init(&_pwm, P0_19);
   
-  //s1 = new PwmOut(P0_18);
- // s2 = new PwmOut(P0_19);
-  
- // s1->period_ms(15);
- // s2->period_ms(15);
-  
-  
   while( 1 )
   {
-    if ( ir_rx == 0 )
-	{
-	  leds = 1<<i;
-	  if ( ++i > 6 ) i = 0;
-	  wait(0.1);
-	}
+   /* if ( ir_rx == 0 ) // button pressed or IR received
+    {
+      leds = 1<<i;
+      if ( ++i > 6 ) 
+        i = 0;
+      wait(0.2);
+    } */
+    
+    if ( button == 0 )
+      play();
+    
     if ( serial_readable( &stdio_uart ) )
     {
-	  char c= '0';
-	  while ( serial_readable( &stdio_uart ) )
-	    c = serial_getc(&stdio_uart); 
-		i = c - '0' - 1;
-		
-	  // if ( i > 0 || i < 0 ) i = 0;
-	  if ( c == 'h' )  
-	    tx(usb_conn, "Hello world 2!\n");
-	  leds = 1<<i;
-	  servo1.pulsewidth_us(700 + 25 * i);
-	  servo2.pulsewidth_us(700 + 25 * i);
-	 // s1->pulsewidth_us(700 + 25 * i);  
-	 // s2->pulsewidth_us(700 + 25 * i);  
-	}
-	//wait(0.01);
+      char c = 0;
+      while ( serial_readable( &stdio_uart ) )
+        c = serial_getc(&stdio_uart); 
+      switch( c )
+      {
+        case 0: 
+          break;
+        case 'n':  // send serialnr as hex value, values are echoed back: so clean the rx FIFO after each TX
+          for(int b=28; b>=0; b-=4) 
+            tx_nibble(usb_conn, serial_nr >> b);
+          wait(0.01);
+          serial_clear(&stdio_uart);
+          break;
+        case 'p':
+          play();
+          break;
+        case 's': // stop
+          break;
+        case 'L': // led value
+          leds = 1<<get_val();
+          break;
+        case 'A': // first servo
+        case 'B': // second servo
+          set_servo( c, get_val() );
+          break;
+        default:
+          break;
+      }
+	  }
   }
-}
+} // do_serial()
 
 
+/**
+*** Main program
+*** work in 2 modes: 
+***   1) HID mode (tried first): receive commands from USB
+***   2) Serial mode (if USB/HID handshake fails within a certain time): Receive commands from serial port
+*** In addition the chip has a USB/MSD bootloader that can be activated by holding the program swith when the chip
+*** comes out of reset. 
+**/
 int main(void) {
-  unsigned short int program[128]; // 256 bytes, need to be word aligned
 
+  wait(2.0);
+
+  serial_nr = iap.read_serial();
+ 
 // enable IR transmitter
   ir_pwm.period(1.0/38000.0);
   ir_pwm.pulsewidth(1.0/72000.0);
 
-    static int count = 0;
 	button.mode(PullUp);
 	uart_rx.mode(PullNone); // disable pull-up, so we can enumerate 
 	
@@ -163,9 +260,9 @@ int main(void) {
 	servo1.pulsewidth_us(700);
 	
 	//Fill the report
-    for(int i = 0; i < 64; i++)
-        send_report.data[i] = i;
-    send_report.length = 64;
+  for(int i = 0; i < 64; i++)
+    send_report.data[i] = i;
+  send_report.length = 64;
 
 	for(int i=0; i<7 && !hid.configured(); i++)
 	{
@@ -173,47 +270,38 @@ int main(void) {
 	  leds = 1<<i;
 	  wait(0.1);
 	}	 
-    if ( !hid.configured() )
-	  do_serial();
-    tic.attach(tic_handler, 0.1);
+  if ( !hid.configured() )
+  do_serial();
+  tic.attach(tic_handler, 0.1);
 	
-    while (1)
+  while (1)
 	{
 	  if ( button == 0 ) // button pressed == LOW 
-	  {
-        iap.read_eeprom( (char*)TARGET_ADDRESS, (char *)program, MEM_SIZE );
-		for(int i=0; i<FAN_STEPS; i++)
-		{
-		    leds = program[i] & 0xFF;
-		    servo1.pulsewidth_us(700 + (program[i]>>8 & 0xFF) );
-		    // leds = ((int)program[i]>>8) & 0xFF;
-			wait(0.1);
-		}
-      }
-	  else
-	    count = 0;
+	    play();
 
-      if (hid.readNB(&recv_report)) 
+    if (hid.readNB(&recv_report)) 
 	  {
 	    read_res++;
-		switch ( recv_report.data[0] ) 
-		{
-		  case 0: // set outputs
-            leds = recv_report.data[1];
-		    servo1.pulsewidth_us(700 + recv_report.data[2]);
-		    servo2.pulsewidth_us(700 + recv_report.data[3]);
-		    send_report.data[1] = button;
-            break;
-		  case 1: // Save data to eeprom 
-		    for(int i=0; i<FAN_STEPS; i++)
-			  program[i] = ((unsigned short int)recv_report.data[1 + 2*i]) | ((unsigned short int)recv_report.data[2 + 2*i] << (unsigned short int)8);
-		     send_report.data[1] = iap.write_eeprom( (char*)program, (char*)TARGET_ADDRESS, MEM_SIZE );
-		    break;
-		  default: // unkown command
-		    break;
+      switch ( recv_report.data[0] ) 
+      {
+        case 0: // set outputs
+          leds = recv_report.data[1];
+          servo1.pulsewidth_us(700 + recv_report.data[2]);
+          servo2.pulsewidth_us(700 + recv_report.data[3]);
+          send_report.data[2] = button;
+          break;
+        case 1: // Save data to eeprom 
+          for(int i=0; i<FAN_STEPS; i++)
+            program[i] = ((unsigned short int)recv_report.data[1 + 2*i]) | ((unsigned short int)recv_report.data[2 + 2*i] << (unsigned short int)8);
+          send_report.data[1] = iap.write_eeprom( (char*)program, (char*)TARGET_ADDRESS, MEM_SIZE );
+          break;
+        case 2: // Run program
+          play();        
+        default: // unkown command
+          break;
         }			
-      }
+    }
 	 // printf("send: %d receive: %d counter: %d\n", send_res, read_res, counter);
 	  wait_ms(1);
 	}
-}
+} // main
--- software/mbed/fanbot_hub/main.cpp
+++ software/mbed/fanbot_hub/main.cpp
@@ -1,34 +1,80 @@
 /*
- * Software serial example
- * Peter Brier
- */
+
+FanBot HUB
+
+RS485 to 24 Serial HUB
+
+Copyright (c) 2013 Peter Brier
+
+This file is part of the FanBot project. 
+See www.kekbot.org
+
+Fanbot is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
 
 #include "mbed.h"
 #include "USBSerial.h"
+#include "pinmap.h"
+#include "serial_api.h"
+
+// externals from serial_api
+extern int stdio_uart_inited;
+extern serial_t stdio_uart;
  
-// externals 
-//extern int stdio_uart_inited;
-//extern serial_t stdio_uart;
  
 //Virtual serial port over USB
 USBSerial usb;
-// Serial pc(USBTX, USBRX); // P0_19, P0_18
-// Serial uart(P1_27, P1_26);
 
-DigitalOut led(P0_7);
+
 DigitalIn button(P0_1);
-
-int i = 0;
+DigitalOut led_a(P1_28);
+DigitalOut led_b(P1_31);
 
 #define BAUD 9600
 #define t_bit (1000000/BAUD) // 9600 bps, 10 microseconds bit time
 
-DigitalInOut pin(P0_20);
-
-// InterruptIn rx(P0_20);
+// Hub pin numbers for 24 ports
+static PinName hub_pin[] = {
+  P0_16, //D1
+  P0_15, //D2 *SWDIO (select pin function 1 for GPIO)
+  P1_22, //D3
+  P0_14, //D4
+  P0_13, //D5
+  P0_12, //D6
+  P0_11, //D7
+  P1_29, //D8
+  P0_22, //D9
+  P0_10, //D10 *SWDCLK (select pin function 1 for GPIO)
+  P0_9,  //D11 
+  P0_8,  //D12
+  P0_23, //D13
+  P1_15, //D14
+  P0_18, //D15
+  P0_19, //D16
+  P1_25, //D17
+  P0_20, //D18
+  P0_2,  //D19
+  P1_26, //D20
+  P1_27, //D21
+  P1_23, //D22
+  P1_24, //D23
+  P0_7,  //D24
+};
 
 // Software UART send. mark is LOW space is HIGH
-void send(char c)
+void send(DigitalInOut  &pin, char c)
 {
   pin.output();
   pin.mode(PullDown);
@@ -37,7 +83,7 @@ void send(char c)
   for(int bit=0; bit<8; bit++) // 8 bits
   { 
     pin = ( ( (unsigned char) c & (1<<bit) ) ? 1 : 0 );
-    led = ( ( (unsigned char) c & (1<<bit) ) ? 1 : 0 );
+    led_a = ( ( (unsigned char) c & (1<<bit) ) ? 1 : 0 );
     wait_us(t_bit);
   }
   pin = 1; // 1 stop bit
@@ -46,8 +92,8 @@ void send(char c)
 }
 
 
-// receive RS232, 9600bps, wait for some time, or return
-char receive(void)
+// receive RS232, 9600bps, wait for some time to see if there is a start bit, or return 0
+char receive(DigitalInOut &pin)
 {
   char c = 0;
   pin.input();
@@ -59,38 +105,175 @@ char receive(void)
   for(int bit=0; bit<8; bit++) // sample 8 bits
   { 
     c |= ( pin == 1 ?  (1<<bit) : 0 );
-    // led = ( ( (unsigned char) c & (1<<bit) ) ? 1 : 0 );
+    led_a = ( ( (unsigned char) c & (1<<bit) ) ? 1 : 0 );
     wait_us(t_bit);
   }
   wait_us(t_bit); // wait for the stopbit to complete
   return c;
 }
 
-int main(void) {
-  static int i;
-  char c;
-  //  serial_init(&stdio_uart, P1_27, P1_26); // default is 9600bps
+class Watchdog {
+public:
+    void kick(float s) {
+     __disable_irq();
+        LPC_WWDT->CLKSEL = 0x1;                // Set CLK src to PCLK
+        //uint32_t clk = 6000/2;    // WD has a fixed /4 prescaler, PCLK default is /4 
+        LPC_WWDT->TC = 0xFFF; // s * (float)clk;         
+        LPC_WWDT->MOD = 0x3;                   // Enabled and Reset        
+        kick();
+         __enable_irq();
+    }
+    
+    void kick() {
+        LPC_WWDT->FEED = 0xAA;
+        LPC_WWDT->FEED = 0x55;
+    }
+};
  
-   // uart.baud(BAUD);
-   // serial.printf("I am a virtual serial port\r\n");
-  led = 1;
+Watchdog w;
+  
+ /**
+ *** iotest()
+ *** Toggle all outputs
+ **/
+ void iotest()
+ {
+  int i=0;
+  w.kick(2.5);
+  wait(1);
+
+  while(1)
+  {
+    i++;
+    led_a = 1;
+    led_b = 0;
+    for(i=0; i<(sizeof(hub_pin)/sizeof(hub_pin[0])); i++)
+    {
+      DigitalOut hub(hub_pin[i]);
+      pin_function(P0_15,1);
+      pin_function(P0_10,1);
+      hub = 1;
+    }
+    wait(0.1);
+    led_a = 0;
+    led_b = (button ? 0 : 1);
+    for(i=0; i<(sizeof(hub_pin)/sizeof(hub_pin[0])); i++)
+    {
+      DigitalOut hub(hub_pin[i]);
+      pin_function(P0_15,1);
+      pin_function(P0_10,1);
+      hub = 0;
+    }
+    wait(0.1);
+  }
+}
+
+/**
+*** Send test string over RS485 and echo received chars
+**/
+void rs485_test()
+{
+  char buf[128];
+  int n=0;
+  //DigitalOut de(P0_17);
+  DigitalOut de(NC);
+  serial_init( &stdio_uart, P1_13, P1_14); // pins: tx, rx
+  LPC_USART->RS485CTRL |= (1<<4) | (1<<5); // Enable bit4: Automatic Direction control
+  pin_function(P0_17,1); // RTS
+  memset(buf,0,sizeof(buf));
+  printf("Hello World!\n");
+  while(1)
+  {    
+    de = 0;
+    while ( serial_readable( &stdio_uart ) && n >= 0 )
+    {     
+     led_a = 1;
+     buf[n] = serial_getc(&stdio_uart);
+     de = 1;
+     serial_putc(&stdio_uart, buf[n]);
+     de = 0;
+     if ( buf[n] == '\r' ) 
+       n = -1;
+     else
+       n++;       
+    }
+    if ( n < 0 )
+    {     
+      led_a = 0;
+      n = 0;
+      de = 1; // driver enable
+      printf("Hello World!\n");
+      printf(buf);
+      memset(buf,0,sizeof(buf));
+    }
+  }
+}
+
+
+/**
+*** Receive via USB virtual COM port and send to one port, and receive chars from that port
+**/
+void single_comm(DigitalInOut &pin)
+{
+  static int i=0;
+  char c;
+  pin_function(P0_15,1);
+  pin_function(P0_10,1);
+
   while(1)
   { 
-   /* while ( serial_readable( &stdio_uart ) ) 
-    {
-      usb.putc( serial_getc(&stdio_uart) );  
-    } */ 
-	while ( usb.available () )
-     {	 
-	   c = usb.getc();
-       send(c);   
-	   usb.putc( c );  
-	 }
-	 c = receive();
-	 if ( c ) usb.putc( c );  
-	 
-	led = ( (i++) & 256 ? 1 : 0 );
-	//wait(0.001);
+	  while ( usb.available () )
+    {	 
+       c = usb.getc();
+       if ( c == '$' ) return;
+       send(pin, c);   
+    }
+    c = receive(pin);
+    if ( c ) usb.putc( c );  
+    led_a = ( (i++) & 256 ? 1 : 0 );
+    led_b = c;
   }
+}
+
+/**
+*** Receive via USB virtual COM port and send to all ports
+**/
+void rs485_comm()
+{
+  static int i;
+  char c=0;
+  while(1)
+  { 
+	  while ( usb.available () )
+    {	 
+       c = usb.getc();
+       if ( c == '#' ) 
+       {
+         i = usb.getc() - 'A';
+         DigitalInOut hub(hub_pin[i]);      
+         single_comm(hub);        
+       }
+       for(i=0; i<(sizeof(hub_pin)/sizeof(hub_pin[0])); i++)
+       {
+         DigitalInOut hub(hub_pin[i]);
+         pin_function(P0_15,1);
+         pin_function(P0_10,1);
+         send(hub, c);   
+       }
+    }
+    if ( c ) usb.putc( c );  
+    led_a = ( (i++) & 256 ? 1 : 0 );
+    led_b = c;
+  }
+}
+
+
+/**
+*** Main function
+**/
+int main(void) {
+ // iotest();
+ //rs485_test();
+ rs485_comm();
 }
 

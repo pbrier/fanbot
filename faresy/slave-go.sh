3ec3749ba5b73f916930ad05bfe0f7069ff9944a
Jean-Claude Wippler <jc@wippler.nl>
2013-06-24 17:23:18 +0200

    avoid quoting issues and run make on reboot

--- faresy/slave-go.sh
+++ faresy/slave-go.sh
@@ -10,4 +10,8 @@ sleep 3
 
 git pull # always get the latest changes before doing any further setup
 
+# build the apps, in case a source change just came in
+(cd slave-lcd && make)
+(cd slave-usbot && make)
+
 sh slave-init.sh

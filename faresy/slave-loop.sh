d8cc06ce09c42c2169186c516140604c2225260b
Jean-Claude Wippler <jc@wippler.nl>
2013-06-25 12:30:56 +0200

    rsync now in background, fix re-registration of same fanbot

--- faresy/slave-loop.sh
+++ faresy/slave-loop.sh
@@ -2,31 +2,29 @@
 
 sudo slave-lcd/lcd -b images/intro.rgb
 
-IP=`cat master-ip.txt`
-RSYNC_PASSWORD=tralala rsync -au incoming/ jcw@$IP::uploads/
-
+# wait a while for a FanBot to be plugged in
 slave-usbot/usbot >/tmp/info.txt
 
 FBID=""
 . /tmp/info.txt
 if [ "$FBID" != "" ]
 then
+  # only reached when readout succeeded
   echo $FBID "$NAME" `date` >>slave-log.txt
+  # show latest image on LCD as long as the FanBot remains plugged in
   while slave-usbot/usbot
-	do
-		if [ -r /tmp/snap.jpg ]
-		then
-		  mv /tmp/snap.jpg /tmp/last.jpg
-			convert /tmp/last.jpg -geometry 240x320 /tmp/last.rgb
-			sudo slave-lcd/lcd -b /tmp/last.rgb
-		fi
-		sleep 0.1
-	done
-	mv /tmp/info.txt incoming/$FBID.txt
-	mv /tmp/last.jpg incoming/$FBID.jpg
-	sudo slave-lcd/lcd -b images/done.rgb
-	while :
-	do
-		slave-usbot/usbot || exit
-	done
+  do
+    if [ -r /tmp/snap.jpg ]
+    then
+      mv /tmp/snap.jpg /tmp/last.jpg
+      convert /tmp/last.jpg -geometry 240x320 /tmp/last.rgb
+      sudo slave-lcd/lcd -b /tmp/last.rgb
+    fi
+    sleep 0.1
+  done
+  # done, prepare to send out a new registration
+  mv -f /tmp/info.txt incoming/$FBID.txt
+  mv -f /tmp/last.jpg incoming/$FBID.jpg
+  sudo slave-lcd/lcd -b images/done.rgb
+  sleep 1
 fi

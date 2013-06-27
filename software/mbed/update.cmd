8c4dd40d57862a5c2114b4ad6c132af1907ec960
pbrier <git@pbrier.nl>
2013-06-27 13:46:06 +0200

    Added binaries

---
+++ software/mbed/update.cmd
@@ -0,0 +1,8 @@
+copy/y ..\..\..\mbed\projects\fanbot fanbot
+copy/y ..\..\..\mbed\projects\fanbot_hub fanbot_hub
+copy/y ..\..\..\mbed\projects\fanbot_test fanbot_test 
+
+copy/y ..\..\..\build\test\LPC11U24\ARM\PB_1\fanbot.bin
+copy/y ..\..\..\build\test\LPC11U24\ARM\PB_2\fanbot_test.bin
+copy/y ..\..\..\build\test\LPC11U24\ARM\PB_3\fanbot_hub.bin
+

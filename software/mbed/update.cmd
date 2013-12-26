e8ef553c5f23db9fbe0c402b9208e9f33fc119d6
pbrier <git@pbrier.nl>
2013-12-26 09:49:35 +0100

    Added audio and biped sources

--- software/mbed/update.cmd
+++ software/mbed/update.cmd
@@ -2,6 +2,9 @@ copy/y ..\..\..\mbed\projects\fanbot fanbot
 copy/y ..\..\..\mbed\projects\fanbot_hub fanbot_hub
 copy/y ..\..\..\mbed\projects\fanbot_test fanbot_test 
 copy/y ..\..\..\mbed\projects\robin robin 
+copy/y ..\..\..\mbed\projects\audio audio 
+copy/y ..\..\..\mbed\projects\biped biped 
+
 
 copy/y ..\..\..\build\test\LPC11U24\ARM\PB_1\fanbot.bin
 copy/y ..\..\..\build\test\LPC11U24\ARM\PB_2\fanbot_test.bin
